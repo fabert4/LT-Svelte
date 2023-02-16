c13d6ae676713c30b6ceee8e8550c396074d7591
T. Faber <tijn.faber@hva.nlgit>
2023-02-16 15:45:15 +0100

    - Loadmaster Update

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -24,7 +24,7 @@
 
 <Table hoverable={true}>
     <caption class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
-        Ready Orders (3)
+        Orders (4)
     </caption>
     <TableHead>
         <TableHeadCell>Identifier</TableHeadCell>
@@ -39,8 +39,8 @@
         <TableBodyRow>
             <TableBodyCell>#4831</TableBodyCell>
             <TableBodyCell>
-                <Button>
-                    <Chevron>Dock {group1}</Chevron>
+                <Button color="red" class="bg-red-500">
+                    <Chevron>Assign Dock</Chevron>
                 </Button>
                 <Dropdown class="w-44 p-3 space-y-3 text-sm">
                     <li>
@@ -66,13 +66,21 @@
                     <img src="https://cdn-icons-png.flaticon.com/512/4021/4021693.png" height="16px" width="16px">
                 </Button>
             </TableBodyCell>
-
         </TableBodyRow>
+        <tr class="font-semibold text-gray-900 dark:text-white bg-white divide-y h-0">
+            <th scope="row" class="py-3 px-6 text-base">Shipment Details: </th>
+            <td class="py-3">Amount: 22 CC</td>
+            <td class="py-3 px-6">Destination: Lidl Köln</td>
+            <th scope="row" class="py-3 px-6 text-base">Notes: </th>
+            <td class="py-3">Assign Dock 3</td>
+            <th scope="row" class="py-3 px-6 text-base">Uploads: </th>
+            <td class="py-3 px-6">None</td>
+        </tr>
         <TableBodyRow>
             <TableBodyCell>#4831</TableBodyCell>
             <TableBodyCell>
-                <Button>
-                    <Chevron>Dock {group1}</Chevron>
+                <Button color="red" class="bg-red-500">
+                    <Chevron>Assign Dock</Chevron>
                 </Button>
                 <Dropdown class="w-44 p-3 space-y-3 text-sm">
                     <li>
@@ -86,6 +94,7 @@
                     </li>
                 </Dropdown>
             </TableBodyCell>
+
             <TableBodyCell>A1234</TableBodyCell>
             <TableBodyCell>7 February 2023 13:59</TableBodyCell>
             <TableBodyCell>B1234</TableBodyCell>
@@ -98,12 +107,20 @@
                     <img src="https://cdn-icons-png.flaticon.com/512/4021/4021693.png" height="16px" width="16px">
                 </Button>
             </TableBodyCell>
-
         </TableBodyRow>
+        <tr class="font-semibold text-gray-900 dark:text-white bg-white divide-y h-0">
+            <th scope="row" class="py-3 px-6 text-base">Shipment Details: </th>
+            <td class="py-3">Amount: 22 CC</td>
+            <td class="py-3 px-6">Destination: Lidl Köln</td>
+            <th scope="row" class="py-3 px-6 text-base">Notes: </th>
+            <td class="py-3 px-6">None</td>
+            <th scope="row" class="py-3 px-6 text-base">Uploads: </th>
+            <td class="py-3 px-6">picture.png</td>
+        </tr>
         <TableBodyRow>
             <TableBodyCell>#4831</TableBodyCell>
             <TableBodyCell>
-                <Button>
+                <Button color="green" class="bg-green-500">
                     <Chevron>Dock {group2}</Chevron>
                 </Button>
                 <Dropdown class="w-44 p-3 space-y-3 text-sm">
@@ -130,49 +147,32 @@
                     <img src="https://cdn-icons-png.flaticon.com/512/4021/4021693.png" height="16px" width="16px">
                 </Button>
             </TableBodyCell>
-
         </TableBodyRow>
-    </TableBody>
-</Table>
-
-
-<Table hoverable={true} class="overflow-y-auto">
-    <caption class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
-        Non-Ready Orders (1)
-    </caption>
-    <TableHead>
-        <TableHeadCell>Identifier</TableHeadCell>
-        <TableHeadCell>Dock</TableHeadCell>
-        <TableHeadCell>Sender Ref</TableHeadCell>
-        <TableHeadCell>Pickup Date</TableHeadCell>
-        <TableHeadCell>Receiver Ref</TableHeadCell>
-        <TableHeadCell>Delivery Date</TableHeadCell>
-        <TableHeadCell>Upload/Notes</TableHeadCell>
-    </TableHead>
-    <TableBody class="divide-y">
+        <tr class="font-semibold text-gray-900 dark:text-white bg-white divide-y h-0">
+            <th scope="row" class="py-3 px-6 text-base">Shipment Details: </th>
+            <td class="py-3">Amount: 22 CC</td>
+            <td class="py-3 px-6">Destination: Lidl Köln</td>
+            <th scope="row" class="py-3 px-6 text-base">Notes: </th>
+            <td class="py-3 px-6">None</td>
+            <th scope="row" class="py-3 px-6 text-base">Uploads: </th>
+            <td class="py-3 px-6">None</td>
+        </tr>
         <TableBodyRow>
             <TableBodyCell>#4831</TableBodyCell>
             <TableBodyCell>
-                <Button><Chevron>Assign Dock</Chevron></Button>
-                <Dropdown class="w-48 overflow-y-auto py-1 h-48">
-                    <DropdownItem class="flex items-center text-base font-semibold gap-2">
-                       1
-                    </DropdownItem>
-                    <DropdownItem class="flex items-center text-base font-semibold gap-2">
-                       2
-                    </DropdownItem>
-                    <DropdownItem class="flex items-center text-base font-semibold gap-2">
-                      3
-                    </DropdownItem>
-                    <DropdownItem class="flex items-center text-base font-semibold gap-2">
-                       4
-                    </DropdownItem>
-                    <DropdownItem class="flex items-center text-base font-semibold gap-2">
-                       5
-                    </DropdownItem>
-                    <DropdownItem class="flex items-center text-base font-semibold gap-2">
-                   6
-                    </DropdownItem>
+                <Button color="green" class="bg-green-500">
+                    <Chevron>Dock {group2}</Chevron>
+                </Button>
+                <Dropdown class="w-44 p-3 space-y-3 text-sm">
+                    <li>
+                        <Radio name="group1" bind:group={group1} value={1}>1</Radio>
+                    </li>
+                    <li>
+                        <Radio name="group1" bind:group={group1} value={2}>2</Radio>
+                    </li>
+                    <li>
+                        <Radio name="group1" bind:group={group1} value={3}>3</Radio>
+                    </li>
                 </Dropdown>
             </TableBodyCell>
             <TableBodyCell>A1234</TableBodyCell>
@@ -181,15 +181,24 @@
             <TableBodyCell>7 February 2023 14:00</TableBodyCell>
             <TableBodyCell>
                 <Button>
-                    <img src="https://cdn-icons-png.flaticon.com/512/126/126477.png" height="16px" width="16px">
+                    <img src="https://icons.veryicon.com/png/o/miscellaneous/standard-general-linear-icon/cloud-upload-14.png" height="16px" width="16px">
                 </Button>
                 <Button>
                     <img src="https://cdn-icons-png.flaticon.com/512/4021/4021693.png" height="16px" width="16px">
                 </Button>
             </TableBodyCell>
-
         </TableBodyRow>
+        <tr class="font-semibold text-gray-900 dark:text-white bg-white divide-y h-0">
+            <th scope="row" class="py-3 px-6 text-base">Shipment Details: </th>
+            <td class="py-3">Amount: 22 CC</td>
+            <td class="py-3 px-6">Destination: Lidl Köln</td>
+            <th scope="row" class="py-3 px-6 text-base">Notes: </th>
+            <td class="">Pick up no earlier than 13:00</td>
+            <th scope="row" class="py-3 px-6 text-base">Uploads: </th>
+            <td class="py-3 px-6">video.mp3</td>
+        </tr>
     </TableBody>
 </Table>
 
-<Button on:click={() => goto('/truckerView/')} class="mt-10 w-24 bottom-0 right-0">Trucker View (TIJDELIJK)</Button>
+
+<!--<Button on:click={() => goto('/truckerView/')} class="mt-10 w-24 bottom-0 right-0">Trucker View (TIJDELIJK)</Button>-->
